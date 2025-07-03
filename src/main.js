d4de9b184b96af167f949253ad3ee3af13c7785b
RyanM3 <ryangmurray3@gmail.com>
2025-07-02 22:30:51 -0500

    removed comsole log message model loaded

--- src/main.js
+++ src/main.js
@@ -37,7 +37,6 @@ controls.enableDamping = true;
 const loader = new GLTFLoader();
 loader.load('bee.glb', (gltf) => {
     const model = gltf.scene;
-    console.log('Model loaded:', model);
     model.position.y = -200; // Adjust the y position to -200
     model.scale.set(1, 1, 1);
     scene.add(model);
